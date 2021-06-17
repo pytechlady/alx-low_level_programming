2b8b19bee536a858be179d22b42205dc711b504a
Adebimpe Afolabi <fumex9910@gmail.com>
2021-06-17 14:06:09 -0700

    Neevr mind

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("size of a char: %1u byte(s)\n, (unsigned long)sizeof(d));
+printf("size of an int : %1u byte(s)\n, (unsigned long)sizeof(a));
+printf("size of a long int: %1u byte(s)\n (unsigned long)sizeof(b));
+printf("size of a long long int: %1u byte(s)\n (unsigned long)sizeof(c));
+printf("size of a float: %1u byte(s)\n (unsigned long)sizeof(f));
+return (0);
+}
