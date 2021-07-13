0cec668416faea0e33812d58b97cbbd9b737893f
Afolabi Adebimpe <fumex9910@gmail.com>
2021-07-13 13:17:06 -0700

    fixed error

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,53 +1,36 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * str_concat - Concatenates two strings of any size
-  * @s1: the first string to concatenate
-  * @s2: the second string to concatenate
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
   *
-  * Return: the two strings concatenated
+  * Return: the string duplicated
   */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	int i = 0, j = 0, k = 0, l = 0;
+	int a = 0, i = 1;
 	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
+	if (str == NULL)
+		return (NULL);
 
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
+	while (str[i])
+	{
 		i++;
+	}
 
-	while (s2[j])
-		j++;
-
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
+	s = malloc((sizeof(char) * i) + 1);
 
 	if (s == NULL)
 		return (NULL);
 
-	j = 0;
-
-	while (k < l)
+	while (a < i)
 	{
-		if (k <= i)
-			s[k] = s1[k];
-
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-
-		k++;
+		s[a] = str[a];
+		a++;
 	}
 
-	s[k] = '\0';
+	s[a] = '\0';
 	return (s);
 }
