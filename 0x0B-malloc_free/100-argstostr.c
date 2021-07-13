499c0807273662dc13d78270ab44df381278dcb8
Afolabi Adebimpe <fumex9910@gmail.com>
2021-07-13 13:39:11 -0700

    Fixed again

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr - concatenate strings pointed to by `av'
- * @ac: number of strings
- * @av: array of strings
- *
- * Return: point to new string, or NULL if failure
+ * argstostr - Concate the areguments.
+ * @ac: Number of arguments.
+ * @av: Arguments.
+ * Return: String concatenated.
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, totalbytes = 0;
-	char *p;
+	int i, j, count = 0;
+	char *tmp;
+	char *tmp1;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; ++i)
 	{
-		for (j = 0; *(*(av + i) + j); ++j)
-			;
-		totalbytes += j;
-		++totalbytes; /* space for newline */
-	}
-	++totalbytes; /* space for string terminator */
-
-	p = (char *) malloc(totalbytes * sizeof(char));
-
-	if (p == NULL)
 		return (NULL);
-	for (i = 0, k = 0; i < ac; ++i, ++k)
-	{
-		for (j = 0; *(*(av + i) + j); ++j, ++k)
-			*(p + k) = *(*(av + i) + j);
-		*(p + k) = '\n';
 	}
-	*(p + k + 1) = '\0';
-	return (p);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != 0; j++)
+		{
+			count++;
+		}
+		count++;
+	}
+	count += 1;
+
+	tmp = malloc(sizeof(char) * count);
+	tmp1 = tmp;
+
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != 0; j++)
+		{
+			*tmp = av[i][j];
+			tmp++;
+		}
+		*tmp = '\n';
+		tmp++;
+	}
+
+	return (tmp1);
 }
