84df98f29215dc93f27ff208973176a9fa9e4334
Afolabi Adebimpe <fumex9910@gmail.com>
2021-07-13 13:15:00 -0700

    Error fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
-/**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
-char *_strdup(char *str)
-{
-	int i, end;
-	char *array;
+#include <stdlib.h>
+#include <stdio.h>
 
-	if (str == NULL)
+/**
+  * str_concat - Concatenates two strings of any size
+  * @s1: the first string to concatenate
+  * @s2: the second string to concatenate
+  *
+  * Return: the two strings concatenated
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
 
-	for (end = 0; end <= *str; end++)
+	j = 0;
+
+	while (k < l)
 	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
 	}
 
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
-		return (NULL);
-	return (array);
+	s[k] = '\0';
+	return (s);
 }
