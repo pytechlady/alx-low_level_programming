2ffcd050a59cf061ed5272c1260f1fc0f9a3f02f
Afolabi Adebimpe <fumex9910@gmail.com>
2021-07-13 13:55:18 -0700

    fixed all

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,86 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
-
-char *_strcat(char *dest, char *src);
-
 /**
- * argstostr - concatenates all the arguments
- * @av: the content
- * @ac: the size of the content
- *
- * Return: a pointer to a new string
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	char *new, *salt;
-	int i, j, k;
+	int i, n, k = 0, len = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		for (i = 0, k = 0 ; i < ac ; i++, k++)
-		{
-			for (j = 0 ; av[i][j] != '\0' ; j++, k++)
-			{
-				;
-			}
-		}
-		new = malloc(sizeof(char) * (k + 1));
-		salt = "\n";
-		for (i = 0 ; i < ac ; i++)
-		{
-			new = _strcat(new, av[i]);
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			if (i + 1 < ac)
-			{
-				new = _strcat(new, salt);
-				if (new == NULL)
-				{
-					return (NULL);
-				}
-			}
-		}
 
-	}
-	new = _strcat(new, salt);
-	if (new == NULL)
+	for (i = 0; i < ac; i++)
 	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
 		return (NULL);
-	}
-	return (new);
-}
 
-/**
- * _strcat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- *
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int a;
-	int b;
-
-	a = 0;
-
-	while (dest[a] != 0)
+	for (i = 0; i < ac; i++)
 	{
-		a++;
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
 	}
-
-	b = 0;
-
-	while (src[b] != 0)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	return (dest);
+	return (str);
 }
